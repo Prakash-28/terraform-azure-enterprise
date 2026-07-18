d32d39897ea02f617fe0ca4b9b52f60c54acab2e
Prakash-28 <prakashvidjiacoumar2002@gmail.com>
2026-07-18 20:39:03 +0530

    Adding Backend configuration

--- backend.tf
+++ backend.tf
@@ -1,1 +1,9 @@
 # configure backend after storage account creation
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "rg-tfstate"
+    storage_account_name = "tfstateab5f887b"
+    container_name       = "tfstate"
+    key                  = "prod.tfstate"
+  }
+}
